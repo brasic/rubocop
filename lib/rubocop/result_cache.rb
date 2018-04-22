7897a41afbda17ddca1f178854781e6c50b35f6d
Carl Brasic <677224+brasic@users.noreply.github.com>
2018-04-21 21:34:58 -0500

    Memoize Dir.pwd
    Dir.pwd incurs a syscall to run.  As long as we don't change the
    directory between calls it can be safely memoized for several orders of
    magnitude in increased performance.
    
        require "benchmark/ips"
    
        $dir = Dir.pwd
    
        Benchmark.ips do |x|
          x.report('pwd', 'Dir.pwd')
          x.report('cached', '$dir')
          x.compare!
        end
    
        ruby bm10.rb
        Warming up --------------------------------------
                         pwd     9.153k i/100ms
                      cached   462.991k i/100ms
        Calculating -------------------------------------
                         pwd     99.667k (± 4.1%) i/s -    503.415k in   5.059904s
                      cached     55.008M (± 7.7%) i/s -    273.165M in   5.002875s
    
        Comparison:
                      cached: 55007789.3 i/s
                         pwd:    99666.8 i/s - 551.92x  slower

--- lib/rubocop/result_cache.rb
+++ lib/rubocop/result_cache.rb
@@ -84,6 +84,7 @@ module RuboCop
                         relevant_options_digest(options),
                         file_checksum(file, config_store))
       @cached_data = CachedData.new(file)
+      @pwd = Dir.pwd
     end
 
     def valid?
@@ -134,7 +135,7 @@ module RuboCop
     def file_checksum(file, config_store)
       stat = File.stat(file)
       digester = Digest::MD5.new
-      digester.update(Dir.pwd + file + stat.mode.to_s + config_store.for(file, stat).signature)
+      digester.update(@pwd + file + stat.mode.to_s + config_store.for(file, stat).signature)
       digester.file(file)
       digester.hexdigest
     rescue Errno::ENOENT
